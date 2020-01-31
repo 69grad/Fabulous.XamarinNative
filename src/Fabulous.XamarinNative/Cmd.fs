4ca178e3214d435f935f12689b2dcd1cbdc02932
Thomas Bandt <bandt@69grad.de>
2020-01-31 15:23:53 +0100

    Cleans up the cmd module

--- src/Fabulous.XamarinNative/Cmd.fs
+++ src/Fabulous.XamarinNative/Cmd.fs
@@ -1,4 +1,4 @@
-﻿// Copyright 2018 Elmish and Fabulous contributors. See LICENSE.md for license.
+﻿// Copyright 2018 Elmish and Fabulous contributors. See https://github.com/fsprojects/Fabulous/blob/master/LICENSE.md for license.
 namespace Fabulous.XamarinNative
 
 /// Dispatch - feed new message into the processing loop
@@ -14,54 +14,50 @@ type Cmd<'msg> = Sub<'msg> list
 [<RequireQualifiedAccess>]
 module Cmd =
     /// None - no commands, also known as `[]`
-    let none : Cmd<'msg> =
+    let none: Cmd<'msg> =
         []
 
     /// Command to issue a specific message
-    let ofMsg (msg:'msg) : Cmd<'msg> =
-        [fun dispatch -> dispatch msg]
+    let ofMsg (msg: 'msg): Cmd<'msg> =
+        [ fun dispatch -> dispatch msg ]
 
     /// Command to issue a specific message, only when Option.IsSome = true
-    let ofMsgOption (msg:'msg option) : Cmd<'msg> =
+    let ofMsgOption (msg: 'msg option): Cmd<'msg> =
         [ fun dispatch ->
-             match msg with
-             | None -> ()
-             | Some msg -> dispatch msg ]
+            match msg with
+            | None -> ()
+            | Some msg -> dispatch msg ]
 
     /// When emitting the message, map to another type
-    let map (f: 'a -> 'msg) (cmd: Cmd<'a>) : Cmd<'msg> =
+    let map (f: 'a -> 'msg) (cmd: Cmd<'a>): Cmd<'msg> =
         cmd |> List.map (fun g -> (fun dispatch -> f >> dispatch) >> g)
 
     /// Aggregate multiple commands
-    let batch (cmds: #seq<Cmd<'msg>>) : Cmd<'msg> =
+    let batch (cmds: #seq<Cmd<'msg>>): Cmd<'msg> =
         cmds |> List.concat
 
     /// Command to call the subscriber
-    let ofSub (sub: Sub<'msg>) : Cmd<'msg> =
-        [sub]
+    let ofSub (sub: Sub<'msg>): Cmd<'msg> =
+        [ sub ]
 
-    let dispatch d (cmd: Cmd<_>) = for sub in cmd do sub d
+    let dispatch d (cmd: Cmd<_>) =
+        for sub in cmd do
+            sub d
 
     /// Command to issue a message at the end of an asynchronous task
-    let ofAsyncMsg (p: Async<'msg>) : Cmd<'msg> =
-        [ fun dispatch -> async { let! msg = p in dispatch msg } |> Async.StartImmediate ]
+    let ofAsyncMsg (p: Async<'msg>): Cmd<'msg> =
+        [ fun dispatch ->
+            async {
+                let! msg = p
+                dispatch msg } |> Async.StartImmediate ]
 
     /// Command to issue a message at the end of an asynchronous task, only when Option.IsSome = true
-    let ofAsyncMsgOption (p: Async<'msg option>) : Cmd<'msg> =
-        [ fun dispatch -> async { 
-            let! msg = p
-            match msg with
-            | None -> ()
-            | Some msg -> dispatch msg } |> Async.StartImmediate ]
- 
-    //let ofAsyncMsgs p : Cmd<_> =
-    //    [ fun dispatch -> p |> AsyncSeq.iter dispatch |> Async.StartImmediate ]
- 
-    //type CmdBuilder() = 
-    //    inherit AsyncSeq.AsyncSeqBuilder()
-    //    member x.Run(p: AsyncSeq<_>) = ofAsyncMsgs p
- 
-//[<AutoOpen>]
-//module CommandBuilder = 
-//    let cmd = Cmd.CmdBuilder()
-
+    let ofAsyncMsgOption (p: Async<'msg option>): Cmd<'msg> =
+        [ fun dispatch ->
+            async {
+                let! msg = p
+                match msg with
+                | None -> ()
+                | Some msg -> dispatch msg
+            }
+            |> Async.StartImmediate ]
