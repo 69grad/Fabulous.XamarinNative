9f80187804ad5e6530fa4a013a59c7c90ebe17e0
Thomas Bandt <bandt@69grad.de>
2020-02-01 14:03:14 +0100

    Simplifies the model

--- samples/MultiPageApp/MultiPageApp/PeopleFormProgram.fs
+++ samples/MultiPageApp/MultiPageApp/PeopleFormProgram.fs
@@ -2,39 +2,41 @@
 
 open Fabulous.XamarinNative
 
-module PeopleFormProgram = 
+module PeopleFormProgram =
     type Model =
-        { NewPerson: Person }
+        { PersonFirstName: string
+          PersonLastName: string }
 
     type Msg =
         | SetFirstname of string
         | SetLastname of string
         | Save
-        | CmdPersistPerson of Person
+        | CmdPersistPerson
 
     type Program(host: IProgramHost) =
-        let initModel() =
-            { NewPerson =
-                  { FirstName = ""
-                    LastName = "" } }
 
-        let init() = initModel(), Cmd.none
-        
-        let persistPerson person =
+        let init() =
+            { PersonFirstName = ""
+              PersonLastName = "" }, Cmd.none
+
+        let persistPerson model =
+            let person =
+                { FirstName = model.PersonFirstName
+                  LastName = model.PersonLastName }
             PeopleRepository.addPerson person
             Messenger.publish "Person added"
             None
 
         let update msg model =
             match msg with
-            | SetFirstname v -> { model with NewPerson = { model.NewPerson with FirstName = v } }, Cmd.none
-            | SetLastname v -> { model with NewPerson = { model.NewPerson with LastName = v } }, Cmd.none
-            | Save -> model, Cmd.ofMsg (CmdPersistPerson model.NewPerson)
-            | CmdPersistPerson person -> model, Cmd.ofMsgOption (persistPerson person)
+            | SetFirstname v -> { model with PersonFirstName = v }, Cmd.none
+            | SetLastname v -> { model with PersonLastName = v }, Cmd.none
+            | Save -> model, Cmd.ofMsg CmdPersistPerson
+            | CmdPersistPerson -> model, Cmd.ofMsgOption (persistPerson model)
 
         let view() =
-            [ "_firstnameTextField" |> Binding.twoWay (fun m -> m.NewPerson.FirstName) (fun v -> SetFirstname v)
-              "_lastnameTextField" |> Binding.twoWay (fun m -> m.NewPerson.LastName) (fun v -> SetLastname v)
+            [ "_firstnameTextField" |> Binding.twoWay (fun m -> m.PersonFirstName) (fun v -> SetFirstname v)
+              "_lastnameTextField" |> Binding.twoWay (fun m -> m.PersonLastName) (fun v -> SetLastname v)
               "_saveButton" |> Binding.msg Save ]
 
         do
