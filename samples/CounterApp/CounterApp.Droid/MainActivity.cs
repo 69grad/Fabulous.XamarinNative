6048b5e35fde11aa8fed42828dea180496fb5ab3
Sebastian Nikles <sebastian.nikles@tum.de>
2020-02-03 14:26:19 +0100

    Implements the first sample app on Android

--- samples/CounterApp/CounterApp.Droid/MainActivity.cs
+++ samples/CounterApp/CounterApp.Droid/MainActivity.cs
@@ -1,23 +1,18 @@
 ﻿using Android.App;
 using Android.OS;
-using Android.Support.V7.App;
 using Fabulous.XamarinNative;
 
 namespace CounterApp.Droid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : FabulousActivity<CounterProgram.Program>
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        public override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
-
             SetContentView(Resource.Layout.activity_main);
-
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-            
             FabulousSetup.initialize();
+
+            base.OnCreate(savedInstanceState);
         }
     }
 }
